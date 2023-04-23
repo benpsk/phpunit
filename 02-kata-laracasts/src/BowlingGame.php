6127b66931b3a21cbee84184a0601dd050b1853e
Ben Gunn <bengunn.dev@gmail.com>
2023-04-24 02:19:34 +0630

    add php formatter

--- 02-kata-laracasts/src/BowlingGame.php
+++ 02-kata-laracasts/src/BowlingGame.php
@@ -2,70 +2,71 @@
 
 namespace App;
 
-class BowlingGame 
+class BowlingGame
 {
-  const FRAMES_PER_GAME = 10;
+    public const FRAMES_PER_GAME = 10;
 
-  protected array $rolls = [];
+    protected array $rolls = [];
 
-  public function roll(int $pins): void
-  {
-    $this->rolls[] = $pins;
-  }
-
-  public function score()
-  {
-    $score = 0;
-    $roll = 0;
-
-    foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
-
-      if ($this->isStrike($roll)) {
-        $score += $this->pinCount($roll) + $this->strikeBonus($roll);
-
-        $roll += 1;
-
-        continue;
-      }
-      
-      $score += $this->defaultFrameScore($roll);
-
-      if ($this->isSpare($roll)) {
-        $score += $this->spareBonus($roll);
-      }
-
-      $roll += 2;
+    public function roll(int $pins): void
+    {
+        $this->rolls[] = $pins;
     }
-    return $score;
-  }
 
-  protected function isStrike(int $roll): bool
-  {
-    return $this->pinCount($roll) === 10;
-  }
+    public function score()
+    {
+        $score = 0;
+        $roll = 0;
 
-  protected function isSpare(int $roll): bool
-  {
-    return $this->defaultFrameScore($roll) === 10;
-  }
+        foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
 
-  protected function defaultFrameScore(int $roll): int
-  {
-    return $this->pinCount($roll) + $this->pinCount($roll + 1);
-  }
-  
-  protected function strikeBonus(int $roll): int
-  {
-    return $this->pinCount($roll +1) + $this->pinCount($roll + 2);
-  }
+            if ($this->isStrike($roll)) {
+                $score += $this->pinCount($roll) + $this->strikeBonus($roll);
 
-  protected function spareBonus(int $roll): int
-  {
-    return $this->pinCount($roll + 2);
-  }
+                $roll += 1;
 
-  protected function pinCount(int $roll): int
-  {
-    return $this->rolls[$roll];
-  }
+                continue;
+            }
+
+            $score += $this->defaultFrameScore($roll);
+
+            if ($this->isSpare($roll)) {
+                $score += $this->spareBonus($roll);
+            }
+
+            $roll += 2;
+        }
+
+        return $score;
+    }
+
+    protected function isStrike(int $roll): bool
+    {
+        return $this->pinCount($roll) === 10;
+    }
+
+    protected function isSpare(int $roll): bool
+    {
+        return $this->defaultFrameScore($roll) === 10;
+    }
+
+    protected function defaultFrameScore(int $roll): int
+    {
+        return $this->pinCount($roll) + $this->pinCount($roll + 1);
+    }
+
+    protected function strikeBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 1) + $this->pinCount($roll + 2);
+    }
+
+    protected function spareBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 2);
+    }
+
+    protected function pinCount(int $roll): int
+    {
+        return $this->rolls[$roll];
+    }
 }
